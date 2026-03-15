f70d5ab6c2b3e4841800c0b853adb2f1cb4dd9dd
Md Shihab Sumon <shihabsumon585@gmail.com>
2026-03-15 12:48:54 +0600

    navbar section complete

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// navbar toggle system
+const menuToggle = document.getElementById('mobile-menu');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+
+    const icon = menuToggle.querySelector('i');
+    if (navMenu.classList.contains('active')) {
+        icon.classList.replace('fa-bars', 'fa-times');
+    } else {
+        icon.classList.replace('fa-times', 'fa-bars');
+    }
+});
+
